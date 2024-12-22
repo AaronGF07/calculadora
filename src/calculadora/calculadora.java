bc01467075e524745c9ef27ec0143a1e53d4269d
AaronGF07 <aarongonzalezf1207@gmail.com>
2024-12-22 16:26:01 +0100

    solicitamos al usuario numeros y operacion a realizar

---
+++ src/calculadora/calculadora.java
@@ -0,0 +1,13 @@
+package calculadora;
+
+public class calculadora {
+
+	public static void main(String[] args) {
+	
+	System.out.println("Ingrese primer numero:");
+	System.out.println("Ingrese segundo numero:");
+	System.out.println("Ingrese operacion (+, -, *, / ): ");
+
+	}
+
+}
