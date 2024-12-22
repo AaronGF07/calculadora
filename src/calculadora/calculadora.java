4408f2db7999898645b39785a1a91ec688f30213
AaronGF07 <aarongonzalezf1207@gmail.com>
2024-12-22 16:40:15 +0100

    se implimenta el scanner para leer los datos del usuario

--- src/calculadora/calculadora.java
+++ src/calculadora/calculadora.java
@@ -1,13 +1,23 @@
 package calculadora;
 
+import java.util.Scanner;
+
 public class calculadora {
 
+	
 	public static void main(String[] args) {
+	Scanner sc=new Scanner (System.in);
+	int num1;
+	int num2;
+	String operacion;
 	
 	System.out.println("Ingrese primer numero:");
+	num1=sc.nextInt();
 	System.out.println("Ingrese segundo numero:");
+	num2=sc.nextInt();
 	System.out.println("Ingrese operacion (+, -, *, / ): ");
-
+	operacion=sc.next();
+	
 	}
 
 }
